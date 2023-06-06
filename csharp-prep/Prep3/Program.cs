196f097f4780341860a3bbb4f85aee0ba45b879b
Andy-8012 <andrewvriley@gmail.com>
2023-06-06 08:14:03 -0600

    Finished Prep 3 Assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,23 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(1,100);
+
+        Console.WriteLine("What is your guess? ");
+        int user_guess = int.Parse(Console.ReadLine());
+
+        while (user_guess != magic_number){
+            if (user_guess < magic_number){
+                Console.WriteLine("Higher");
+                user_guess = int.Parse(Console.ReadLine());
+            }
+            else{
+                Console.WriteLine("Lower");
+                user_guess = int.Parse(Console.ReadLine());
+            }
+        }
+        Console.WriteLine("You guess it!");
     }
 }
