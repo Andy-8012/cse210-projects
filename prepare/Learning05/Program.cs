7e87007a7eeff6f5bbf314611b75986804d35ed7
Andy-8012 <andrewvriley@gmail.com>
2023-07-06 15:27:04 -0600

    Learning 05 Complete

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,23 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("pink", 4);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle ("blue", 4, 5);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("red", 3);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes){
+            Console.WriteLine($"Color:  {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea()}");
+        }
+
+        
     }
 }
